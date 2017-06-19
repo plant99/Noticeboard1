4330078423585efa2bb0729f38ee4dcc77befa2c
shivPadhi <shivashispadhi@gmail.com>
2017-06-19 13:17:31 +0530

    Removed dummy user and notice insertion

--- config/mongoInit.js
+++ config/mongoInit.js
@@ -10,30 +10,6 @@ module.exports.initDb = function(){
 
     	User = require('../models/User')
     	Notice = require('../models/Notice')
-
-    	var user = new User({
-    		username: 'chutiya',
-    		password: 'Chutiya',
-    		type: 'ChutiyA'
-    	})
-
-    	var notice = new Notice({
-    		content: 'This is piece of shit brother'
-    	})
-
-    	user.save(function(err, user){
-    		if(err){console.log(err)}
-    		else{
-    			console.log(user)
-    		}
-    	})
-
-    	notice.save(function(err, notice){
-    		if(err){console.log(err)}
-    		else{
-    			console.log(user)
-    		}
-    	})    	
     })
 }
 
