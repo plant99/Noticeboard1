f723d121f6f7f3032d94a6900509d065b7064c04
shivPadhi <shivashispadhi@gmail.com>
2017-06-19 16:58:10 +0530

    Modify process to give unique username for registrations

--- routes/signUpHandler.js
+++ routes/signUpHandler.js
@@ -3,13 +3,21 @@ var router = express.Router() ;
 
 router.post('/',function(req,res,next){
 	console.log('Signup hai bhai')
-	var user = User({username: req.body.username, password: req.body.password, type: req.body.type})
-	user.save(function(err,user){
-		console.log(user)
-	}) ;
-	setTimeout(function(){
-		res.redirect('/login')
-	},1000)
+	console.log(req.body.username)
+	User.find({username:req.body.username},function(err, user){
+		if(user.length){
+			console.log(user)
+			res.json({success: false, message: 'user exists, please login'})
+		}else{
+			var user = User({username: req.body.username, password: req.body.password, type: req.body.type})
+			user.save(function(err,user){
+				console.log(user)
+			}) ;
+			setTimeout(function(){
+				res.redirect('/login')
+			},1000)
+		}
+	})
 })
 
 router.get('/',function(req, res, next){
