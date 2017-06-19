6dccc665b7b00847292e0d6faf334accb69ddbf9
shivPadhi <shivashispadhi@gmail.com>
2017-06-19 16:15:47 +0530

    Add route and logic handler for signup, save the user.

--- routes/signUpHandler.js
+++ routes/signUpHandler.js
@@ -1,8 +1,19 @@
 var express = require('express')
 var router = express.Router() ;
 
-router.get('/',function(req,res,next){
-	res.end('Sign Up')
+router.post('/',function(req,res,next){
+	console.log('Signup hai bhai')
+	var user = User({username: req.body.username, password: req.body.password, type: req.body.type})
+	user.save(function(err,user){
+		console.log(user)
+	}) ;
+	setTimeout(function(){
+		res.redirect('/login')
+	},1000)
+})
+
+router.get('/',function(req, res, next){
+	res.render('signup')
 })
 
 module.exports = router ;
