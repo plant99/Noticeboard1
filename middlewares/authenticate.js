84636660dd1752f36f9d31dc22a3aa1319036096
shivPadhi <shivashispadhi@gmail.com>
2017-06-19 13:18:36 +0530

    Check also among cookies for token

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,4 +1,18 @@
+var jwt = require('jsonwebtoken')
+
 module.exports = function(req,res,next){
-	console.log('Authenticate')
-	next()	 ;
+	console.log(req.body)
+	var token = req.body.token || req.headers['x-access-token'] || req.query.token || req.cookies.token
+	console.log(token)
+	if(token){
+		jwt.verify(token, superSecret, function(err, decoded){
+			if(err){
+				return res.json({ success: false, message: 'Failed to authenticate token.' });
+			}
+			console.log(decoded) ;
+			next() ;
+		})
+	}else{
+		res.end('No permission, sorry')
+	}
 }
