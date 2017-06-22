da58639534c56e323b198c9dd0690f5506c5fe27
shivPadhi <shivashispadhi@gmail.com>
2017-06-22 07:41:47 +0530

    Console random things to debug

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,11 +1,13 @@
 var jwt = require('jsonwebtoken')
 
 module.exports = function(req,res,next){
+	console.log('Chutiya')
 	console.log(req.body)
 	var token = req.body.token || req.headers['x-access-token'] || req.query.token || req.cookies.token
 	console.log(token)
 	if(token){
 		jwt.verify(token, superSecret, function(err, decoded){
+			console.log(err) ;
 			if(err){
 				res.render('error', {
 					message: 'Login wasn\'t a success, try again'
