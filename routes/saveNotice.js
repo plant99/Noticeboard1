1800ae478a424695aa0b5c12f35131043228db8c
shivPadhi <shivashispadhi@gmail.com>
2017-06-23 07:09:36 +0530

    save as can_be_displayed: false , when user is moderated

--- routes/saveNotice.js
+++ routes/saveNotice.js
@@ -5,7 +5,12 @@ router.post('/',function(req, res, next){
 	console.log(req)
 	if(req.decoded._doc.type === 'teacher' || req.decoded._doc.type === 'cr'){
 		console.log(req.body.title)
-		var notice  = new Notice({header: req.body.title, content: req.body.content}) ;
+		if(req.decoded._doc.moderated === false){
+			var notice  = new Notice({header: req.body.title, content: req.body.content, can_be_displayed:true}) ;
+		}else if(req.decoded._doc.moderated === true){
+			var notice  = new Notice({header: req.body.title, content: req.body.content, can_be_displayed:false}) ;
+		}
+
 		notice.save(function(err, notice1){
 			console.log(notice1)
 			res.json({success:true})
