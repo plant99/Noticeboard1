54c8ec22e3059a5a9474f031358089097c82bd73
shivPadhi <shivashispadhi@gmail.com>
2017-06-24 11:38:58 +0530

    Add property to check the notice name for availability before saving, save image

--- routes/saveNotice.js
+++ routes/saveNotice.js
@@ -3,18 +3,36 @@ var router = express.Router() ;
 var fileUpload = require('express-fileupload');
 
 router.post('/',function(req, res, next){
-	console.log(req)
+	console.log(req.files.image);
 	if(req.decoded._doc.type === 'teacher' || req.decoded._doc.type === 'cr'){
-		console.log(req.body.title)
-		if(req.decoded._doc.moderated === false){
-			var notice  = new Notice({header: req.body.title, content: req.body.content, can_be_displayed:true}) ;
-		}else if(req.decoded._doc.moderated === true){
-			var notice  = new Notice({header: req.body.title, content: req.body.content, can_be_displayed:false}) ;
-		}
+		Notice.findOne({header: req.body.title}, function(err, noticeCheck){
+			if(noticeCheck){
+				res.render('addNotice', {message:'Notice with the same header exists.'})
+			}else{
+				var sampleFile = req.files.image;
+				console.log(sampleFile.mimetype)
+				if(req.decoded._doc.moderated === false){
+					var notice  = new Notice({header: req.body.title, content: req.body.content, can_be_displayed:true}) ;
+				}else if(req.decoded._doc.moderated === true){
+					var notice  = new Notice({header: req.body.title, content: req.body.content, can_be_displayed:false}) ;
+				}
+				if(sampleFile){
+					sampleFile.mv(__dirname+'/../usersImage/'+req.body.title+'.png', function(err) {
+					    if (err)
+					      console.log(err)
+					});
+				}
 
-		notice.save(function(err, notice1){
-			console.log(notice1)
-			res.json({success:true})
+				notice.save(function(err, notice1){
+					if(err){
+						res.json({success: false})
+					}else{
+						console.log(notice1)
+						res.json({success:true})
+					}
+				})
+
+			}
 		})
 	}else{
 		res.json({success:false})
